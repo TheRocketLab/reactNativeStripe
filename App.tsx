7edabb2e8d73b8bb1c6faec54bb5c513256b5ebe
Davin McDonald <davin.mcdonald@rocketlab.com.au>
2023-10-05 14:51:41 +1100

    copy of reactNativeMockLogin

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginFlow from './src/LoginFlow';
+
+const Stack = createStackNavigator();
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login Flow" component={LoginFlow} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
